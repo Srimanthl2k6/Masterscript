6baa7cc45e9f8f6947160f2668728eece94a2400
Srimanthl2k6 <clashroyalehope2k5@gmail.com>
2026-05-20 13:38:11 +0530

    Prepare v0.1.1 release

--- src/lib/download.test.ts
+++ src/lib/download.test.ts
@@ -4,7 +4,7 @@ import { MASTER_SCRIPT_DOWNLOAD_URL, shouldShowDownloadButton } from './download
 describe('download links', () => {
   it('points users to the published Windows installer release asset', () => {
     expect(MASTER_SCRIPT_DOWNLOAD_URL).toBe(
-      'https://github.com/Srimanthl2k6/Masterscript/releases/download/v0.1.0/MasterScript.Setup.0.1.0.exe',
+      'https://github.com/Srimanthl2k6/Masterscript/releases/download/v0.1.1/MasterScript.Setup.0.1.1.exe',
     )
   })
 
--- src/lib/download.ts
+++ src/lib/download.ts
@@ -1,4 +1,4 @@
 export const MASTER_SCRIPT_DOWNLOAD_URL =
-  'https://github.com/Srimanthl2k6/Masterscript/releases/download/v0.1.0/MasterScript.Setup.0.1.0.exe'
+  'https://github.com/Srimanthl2k6/Masterscript/releases/download/v0.1.1/MasterScript.Setup.0.1.1.exe'
 
 export const shouldShowDownloadButton = (isElectron: boolean) => !isElectron
